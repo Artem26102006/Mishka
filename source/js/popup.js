c332c730a99b8a29d7ba0a7d460f6967278f88ad
Artem <kazuta.artem@gmail.com>
2022-07-23 23:35:20 +0300

    made-some-thing-from-styleguide

---
+++ source/js/popup.js
@@ -0,0 +1,11 @@
+let button_show = document.querySelector(".descrip-product__button");
+let button_unshow = document.querySelector(".popup__button");
+let popup = document.querySelector(".popup");
+
+button_show.addEventListener('click', function(){
+  popup.style.display = 'block';
+})
+
+button_unshow.addEventListener('click', function(){
+  popup.style.display = 'none';
+})
